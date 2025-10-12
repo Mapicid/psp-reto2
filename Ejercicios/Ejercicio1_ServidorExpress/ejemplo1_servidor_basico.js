f9a715cf74e15891d06a697eaed8e9c961969053
Mapicid <mpdelcid@cebanc.com>
2025-10-12 18:03:59 +0200

    Update ejemplo1_servidor_basico.js

--- Ejercicios/Ejercicio1_ServidorExpress/ejemplo1_servidor_basico.js
+++ Ejercicios/Ejercicio1_ServidorExpress/ejemplo1_servidor_basico.js
@@ -1,1 +1,21 @@
+// Importamos el módulo Express
+import express from "express";
+
+// Creamos una aplicación Express
+const app = express();
+
+// Definimos el puerto donde escuchará el servidor
+const port = 3000;
+
+// Definimos una ruta principal ("/")
+// Cuando alguien accede a http://localhost:3000
+// el servidor responde con un mensaje de texto
+app.get("/", (req, res) => {
+  res.send("¡Servidor funcionando correctamente con Express!");
+});
+
+// Iniciamos el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
 
